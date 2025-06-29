2f55295fc3df05be5e366a24ba3d8e78b8ff6a0a
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-29 22:23:11 +0600

    signup function done and also install bcrypt for hashing password

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,8 +1,36 @@
-export const signup = (req, res) =>{
-    const data = req.body;
-    console.log(data)
-    res.send(data)
-}
+import { User } from "../model/user.model.js";
+import bcrypt from 'bcryptjs';
+
+export const signup = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(401).json({ errors: "User already exists" });
+    }
+     
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    return res.status(201).json({ message: "User signup successful" });
+
+  } catch (error) {
+    console.error("Error in signup:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 
 export const login = (req, res) =>{
     console.log("login in function!!");
