47ec5ef1c68464ac1f95b97940bd9873ea2bd87a
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-06-30 16:55:44 +0600

    Writing documentation

---
+++ server/documentation.js
@@ -0,0 +1,9 @@
+// step 1: Install express.js and also create initial server 
+// step 2: Install dotenv for store secret data in .env file
+// step 3: install mongoose for create schema 
+// step 4: for schema create model folder inside the folder create user.model.js and inside this file create user schema 
+// step 5: then creat Controller folder here write all the user function and export inside the User.controller.js
+// step 6: Create a routes folder inside the User.route.js file 
+// step 7: Connect MongoDB database
+// step 8: For login use JWT authentication. then need to create config.js file in root 
+// step 9: for logout perspective when user login set token in cookies and then in logout function clear this cookie 
