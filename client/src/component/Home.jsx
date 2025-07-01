be49daffe535f49b3ddeccc2196014720f2b64e7
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-07-01 22:50:41 +0600

    Component initial setup done

---
+++ client/src/component/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import Promt from './promt';
+
+const Home = () => {
+    return (
+        <div className='flex h-screen bg-[#1e1e1e] text-white'>
+            {/* sidebar  */}
+            <div className='w-64 bg-[#232327]'>
+                <Sidebar></Sidebar>
+            </div>
+
+            {/* promt  */}
+            <div className='flex-1 flex flex-col w-full'>
+                 <div className='flex-1 flex items-center justify-center px-6'>
+                    <Promt />
+
+                 </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ client/src/component/Promt.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Promt = () => {
+    return (
+        <div>
+            <h1>promt</h1>
+        </div>
+    );
+};
+
+export default Promt;
