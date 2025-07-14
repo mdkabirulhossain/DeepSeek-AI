5f9af7099cf974aabbfdc6f82e1b551c0a7aa0cf
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-07-14 21:09:59 +0600

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './component/Home'
+import Login from './component/login'
+import Signup from './component/signup'
 
 function App() {
   return (
     <div>
-      <Home />
+      
+      <Routes>
+          <Route path='/' element= {<Home />} />
+          <Route path='/login' element= {<Login />} />
+          <Route path='/signup' element= {<Signup />} />
+      </Routes>
     </div>
 
   )
