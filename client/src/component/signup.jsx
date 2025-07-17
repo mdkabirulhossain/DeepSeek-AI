5cb115c1e69313d5adb00abf83b541447810c505
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-07-17 21:41:52 +0600

    Signup page make logical

--- client/src/component/signup.jsx
+++ client/src/component/signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { data, Link, useNavigate } from 'react-router-dom';
 import { FaRegEye } from "react-icons/fa";
+import axios from 'axios'
 
 const Signup = () => {
 
@@ -13,7 +14,8 @@ const Signup = () => {
 
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
-    
+    const navigate = useNavigate()
+
      const handleChange = (e) =>{
         const value = e.target.value
         const name = e.target.name
@@ -24,14 +26,26 @@ const Signup = () => {
         })
     }
 
-    const handleSignup = () =>{
+    const handleSignup =async () =>{
+        setLoading(true)
+        setError("")
         try{
-
+            const data = await axios.post("http://localhost:3000/api/v1/user/signup", {
+                firstName:formData.firstName,
+                lastName:formData.lastName,
+                email:formData.email,
+                password: formData.email
+            },{
+                withCredentials:true
+            })  
+            alert(data.message || "Signup Succeded")
+            navigate("/login")
         }catch(error){
-
+            const msg = error?.response?.data?.errors || "Signup Failed"
+            setError(msg)
         }
         finally{
-
+            setLoading(false)
         }
     }
 
@@ -42,25 +56,27 @@ const Signup = () => {
             <div className='bg-[#1e1e1e] text-white w-full max-w-md rounded-2xl p-6 shadow-lg'>
                 <h1 className='text-center'>SignUp</h1>
                 <div className='mb-4 mt-2'>
-                    <input className='w-full bg-transparent border border-gray-600 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#7a6ff0]' type="text" placeholder='firstname' name="firstName" value={formData.firstName} onChange={handleChange} id="" />
+                    <input className='w-full bg-transparent border border-gray-600 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#7a6ff0]' type="text" placeholder='firstname' name="firstName" value={formData.firstName} onChange={handleChange}  />
                 </div>
                 <div className='mb-4 mt-2'>
-                    <input className='w-full bg-transparent border border-gray-600 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#7a6ff0]' type="text" placeholder='lastname' name="lastName" value={formData.lastName} onChange={handleChange} id="" />
+                    <input className='w-full bg-transparent border border-gray-600 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#7a6ff0]' type="text" placeholder='lastname' name="lastName" value={formData.lastName} onChange={handleChange} />
                 </div>
                 <div className='mb-4 mt-2'>
-                    <input className='w-full bg-transparent border border-gray-600 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#7a6ff0]' type="email" placeholder='email' name="email" value={formData.email} onChange={handleChange} id="" />
+                    <input className='w-full bg-transparent border border-gray-600 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#7a6ff0]' type="email" placeholder='email' name="email" value={formData.email} onChange={handleChange} />
                 </div>
                 <div className='mb-4 mt-2 relative'>
-                    <input className='w-full bg-transparent border border-gray-600 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#7a6ff0]' type="password" placeholder='password' name="password" value={formData.password} onChange={handleChange} id="" />
+                    <input className='w-full bg-transparent border border-gray-600 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#7a6ff0]' type="password" placeholder='password' name="password" value={formData.password} onChange={handleChange} />
 
                      <FaRegEye  className='absolute right-3 top-3'/>
                 </div>
                 {/* Error message  */}
-                <p className='text-red-600 text-sm mb-4 '>Error</p>
+                {
+                    error && <p className='text-red-600 text-sm mb-4 '>{error}</p>
+                }
                 <p className='text-xs text-gray-500 mt-4 mb-6'>By signing up or logging in, you consent to DeepSeek 
                  <span className='underline mx-1'>Terms of Use</span> <span className='underline'>Privacy Policy</span>
                 </p>
-                <button className='w-full bg-[#7a6ff6] hover:bg-[#6c61a6] text-white font-semibold py-3 rounded-lg transitions disabled:opacity-50' onClick={handleSignup}>SignUp</button>
+                <button className='w-full bg-[#7a6ff6] hover:bg-[#6c61a6] text-white font-semibold py-3 rounded-lg transitions disabled:opacity-50' onClick={handleSignup} disabled={loading}>SignUp</button>
                 <div className='flex justify-between mt-4 text-sm'>
                     <p className='text-[#7a6ff6]'>Already Registered?</p>
                     <Link className='text-[#7a6ff6] hover:underline' to={'/login'}>Login</Link>
